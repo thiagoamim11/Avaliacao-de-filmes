18e5ba659e40c854e4ba851cab752b727de68d49
Thiago Amim <thiagoamim11@gmail.com>
2024-03-05 16:36:26 -0300

    Sidebar em construção

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,25 @@
 import './Global.css'
-import { BrowserRouter as Router, Routes,Route} from 'react-router-dom';
-import TelaDeLogin from './components/TelaDeLogin';
-import TelaPrincipal from './components/TelaPrincipal';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import TelaDeLogin from './components/TelaDeLogin'
+import TelaPrincipal from './components/TelaPrincipal'
+import { Sidebar } from './components/Sidebar'
 
 
 function App() {
-  
+
   return (
-    <Router>
-     <Routes>
+    <div>
+      <Sidebar/>
+      <Router>
+        <Routes>
 
-        <Route path="/" element={<TelaDeLogin/>}/>
-        <Route path="/TelaPrincipal" element={<TelaPrincipal/>}/>
+          <Route path="/" element={<TelaDeLogin />} />
+          <Route path="/TelaPrincipal" element={<TelaPrincipal />} />
 
-     </Routes>
-    </Router>
-    
-    
+        </Routes>
+      </Router>
+    </div>
+
   )
 }
 
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,15 @@
+import './Sidebar.module.css'
+
+
+export function Sidebar() {
+    return (
+        <header>
+            <h4>Avaliação de <strong>Filmes</strong> & <strong>Series</strong></h4>
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">Filmes</a></li>
+                <li><a href="">Series</a></li>
+            </ul>
+        </header>
+    )
+}

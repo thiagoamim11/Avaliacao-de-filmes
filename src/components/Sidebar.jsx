d7047fe0805317d1c528f7e4843b93ec953f797c
Thiago Amim <thiagoamim11@gmail.com>
2024-03-06 15:26:34 -0300

    Tentando resolver o problema da seta

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,15 +1,36 @@
 import './Sidebar.module.css'
+import React, { useState } from 'react'
+import { BsFillCaretDownFill } from 'react-icons/bs'
 
 
 export function Sidebar() {
+    const [showGeneroDropdown, setShowGeneroDropdown] = useState(false)
+    const genero = ["Terror", "Comédia", "Ação", "Suspense"]
+
+
     return (
         <header>
             <h4>Avaliação de <strong>Filmes</strong> & <strong>Series</strong></h4>
             <ul>
-                <li><a href="">Home</a></li>
-                <li><a href="">Filmes</a></li>
-                <li><a href="">Series</a></li>
+                <li><a href=''>Home</a></li>
+                <li><a href=''>Filmes</a></li>
+                <li><a href=''>Series</a></li>
+
+                <li>
+                    <div>
+                        <a href="">Categoria</a>
+                        
+                       {/* <BsFillCaretDownFill className='seta' onClick={() => setShowGeneroDropdown(!showGeneroDropdown)} /> {/* Esse codigo é do icone da setinha para baixo */}
+                    </div>
+                    {showGeneroDropdown && (
+                        <ul>
+                            {genero.map(genero => (
+                                <li key={genero}>{genero}</li>
+                            ))}
+                        </ul>
+                    )}
+                </li>
             </ul>
-        </header>
+        </header >
     )
 }

57b9003fbc58b3b28ee36ed9a2651daf42344ab8
Thiago Amim <thiagoamim11@gmail.com>
2024-03-13 13:34:49 -0300

    Corrigindo conflito parte 2

--- src/components/TelaPrincipal.jsx
+++ src/components/TelaPrincipal.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './TelaPrincipal.module.css';
+import { Sidebar } from './Sidebar';
+
 
 
 export function TelaPrincipal() {
     return (
         <div>
-        <h1>Tela Principal</h1>
-        <Link to = "/">
-         <button>Tela De Login</button>
-        </Link>
+            {/*
+            <h1>Tela Principal</h1>
+            <Link to="/">
+                <button>Tela De Login</button>
+            </Link>
+    esse trecho de codigo esta bugando o sidebar, temos que descobrir como usar os dois juntos*/} 
         </div>
     )
 }

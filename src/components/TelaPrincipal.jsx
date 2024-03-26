923086421b360a8ec0d0ae8cc58c2512d8e1b6e6
thiagooterolima <72893583+thiagooterolima@users.noreply.github.com>
2024-03-26 14:18:45 -0300

    carrousel finalizado

--- src/components/TelaPrincipal.jsx
+++ src/components/TelaPrincipal.jsx
@@ -1,29 +1,89 @@
 import React from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 import { Link } from 'react-router-dom';
 import styles from './TelaPrincipal.module.css';
 import { Sidebar } from './Sidebar';
 
-export function TelaPrincipal() {
+export function TelaPrincipal({ draggable }) {
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 3000,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 200,
+        arrows: false,
+        draggable: draggable,
+        
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    };
+
     return (
-        <div>   {/* fazer um carrocel aqui  */}
+        <div>
             <Sidebar />
             <main>
-                <div className={styles.carrocel}>
-                    <ul>
-                        <li><img src='cartaz.webp' alt="Imagem 1" /></li>
-                        <li><img src='cartaz1.jpg' alt="Imagem 2" /></li>
-                        <li><img src='cartaz2.jpg' alt="Imagem 3" /></li>
-                        <li><img src='cartaz3.png' alt="Imagem 4" /></li>
-                    </ul>
-                </div>
+                <div className={styles.carousel}>
+                <div className={styles.sliderWrapper}>
+                    <div className={styles.titulo}>
+                    <h1>Filmes Em Destaque</h1>
+                    </div>
 
+                    <Slider {...settings}>
+                        <div className={styles.imageWrapper}>
+                            <img src='https://capas-p.imagemfilmes.com.br/164747_000_p.jpg' className={styles.carouselImage} draggable={draggable} />
+                        </div>
+                        <div className={styles.imageWrapper}>
+                            <img src='https://capas-p.imagemfilmes.com.br/164856_000_p.jpg' className={styles.carouselImage} draggable={draggable} />
+                        </div>
+                        <div className={styles.imageWrapper}>
+                            <img src='https://capas-p.imagemfilmes.com.br/164869_000_p.jpg' className={styles.carouselImage} draggable={draggable} />
+                        </div>
+                        <div className={styles.imageWrapper}>
+                            <img src='https://capas-p.imagemfilmes.com.br/164859_000_p.jpg' className={styles.carouselImage} draggable={draggable} />
+                        </div>
+                        <div className={styles.imageWrapper}>
+                            <img src='https://capas-p.imagemfilmes.com.br/164815_000_p.jpg' className={styles.carouselImage} draggable={draggable} />
+                        </div>
+
+
+                        {/* Adicione mais imagens conforme necessário */}
+                    </Slider>
+                    </div>
+                </div>
+    
                 <section>
-                    <div className={styles.areaDeLogin}> {/*Login */}
+                    <div className={styles.loginArea}> {/* Login */}
                         <div>
-                            <img src='filme1.png' />
+                            <img src='filme1.png' alt="Filme 1" />
                             <h4>Avaliação de <strong>Filmes</strong> & <strong>Séries</strong></h4>
                         </div>
-                        
                         
                         <input type='text' placeholder='Login ou e-mail' />
                         <input type='text' placeholder='Senha' />
@@ -32,5 +92,5 @@ export function TelaPrincipal() {
                 </section>
             </main>
         </div>
-    );
+    )
 }

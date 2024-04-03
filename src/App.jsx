2fbdc02c7c0e41540efbab7583b40f7b80226602
Thiago Amim <thiagoamim11@gmail.com>
2024-04-02 21:34:43 -0300

    Tela de cadastro pronta

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './Global.css';
 import { TelaPrincipal } from './components/TelaPrincipal';
 import { Sidebar } from './components/Sidebar';
+import { TelaCadastro } from './components/TelaCadastro';
 
 function App() {
   return (    
--- src/components/TelaCadastro.jsx
+++ src/components/TelaCadastro.jsx
@@ -1,9 +1,31 @@
 import React from "react"
+import { Sidebar } from "./Sidebar"
+import styles from "./TelaCadastro.module.css"
+
 
 
 export function TelaCadastro() {
 
-    return(
-        <h1>hello world</h1>
+    return (
+        <div>
+            <Sidebar />
+            <main className={styles.main5}>
+                <div className={styles.cadastros5}>
+                    <h1 className={styles.titulo5}>C A D A S T R O</h1>
+                    <div>                
+                        <label className={styles.label5}>Email</label>        
+                        <input type='text' placeholder='Digite seu email' />
+
+                        <label className={styles.label5}>Senha</label>
+                        <input type='text' placeholder='Digite uma senha' />
+
+                        <label className={styles.label5}>Confirmar senha</label>
+                        <input type='text' placeholder='Digite a senha novamente' />
+                        <button className={styles.button5}>Confirmar</button>
+                    </div>
+                </div>
+            </main>
+        </div>
+
     )
 }

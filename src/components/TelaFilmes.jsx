44e188f483aa722e760164ace0970f551500caf4
thiagooterolima <72893583+thiagooterolima@users.noreply.github.com>
2024-04-16 12:47:35 -0300

    Tela de Filmes quase pronta

--- src/components/TelaFilmes.jsx
+++ src/components/TelaFilmes.jsx
@@ -1,19 +1,102 @@
-import React from "react"
-import { Sidebar } from "./Sidebar"
-import styles from "./TelaFilmes.module.css"
-
-
+import React from "react";
+import { Sidebar } from "./Sidebar";
+import styles from "./TelaFilmes.module.css";
 
 export function TelaFilmes() {
-
   return (
     <div>
       <Sidebar />
       <div className={styles.container}>
         <h1 className={styles.titulo}>FILMES</h1>
-      </div>
 
+        <div className={styles.fila1}> 
+  <img 
+    src="https://capas-p.imagemfilmes.com.br/164747_000_p.jpg" 
+    alt="Capa do filme" 
+    style={{ width: '15%', height: 'auto',margin:'10px' }} // Definindo largura de 100% e altura automática
+  />
+
+   <img 
+    src="https://capas-p.imagemfilmes.com.br/164856_000_p.jpg" 
+    alt="Capa do filme" 
+    style={{ width: '15%', height: 'auto',margin:'10px' }} // Definindo largura de 100% e altura automática
+  />
+
+   <img 
+    src="https://capas-p.imagemfilmes.com.br/164869_000_p.jpg" 
+    alt="Capa do filme" 
+    style={{ width: '15%', height: 'auto',margin:'10px' }} // Definindo largura de 100% e altura automática
+  />
+
+ <img 
+    src="https://capas-p.imagemfilmes.com.br/164859_000_p.jpg" 
+    alt="Capa do filme" 
+    style={{ width: '15%', height: 'auto',margin:'10px' }} // Definindo largura de 100% e altura automática
+  />
+
+ <img 
+    src="https://capas-p.imagemfilmes.com.br/164815_000_p.jpg" 
+    alt="Capa do filme" 
+    style={{ width: '15%', height: 'auto',margin:'10px' }} // Definindo largura de 100% e altura automática
+  />
+
+<img 
+    src="https://capas-p.imagemfilmes.com.br/164099_000_p.jpg" 
+    alt="Capa do filme" 
+    style={{ width: '15%', height: 'auto',margin:'10px' }} // Definindo largura de 100% e altura automática
+  />
+</div>
+
+
+       
+        <div className={styles.fila2}>
+
+        <img 
+          src="https://capas-p.imagemfilmes.com.br/164065_000_p.jpg" 
+          alt="Capa do filme" 
+          style={{ width: '15%', height: 'auto',margin:'10px' }} // Definindo largura de 100% e altura automática
+        />
+
+        <img 
+          src="https://capas-p.imagemfilmes.com.br/164114_000_p.jpg" 
+          alt="Capa do filme" 
+          style={{ width: '15%', height: 'auto',margin:'10px' }} // Definindo largura de 100% e altura automática
+        />
+
+         <img 
+          src="https://capas-p.imagemfilmes.com.br/164093_000_p.jpg" 
+          alt="Capa do filme" 
+          style={{ width: '15%', height: 'auto',margin:'10px' }} // Definindo largura de 100% e altura automática
+        />
+
+        
+        <img 
+          src="https://capas-p.imagemfilmes.com.br/164091_000_p.jpg" 
+          alt="Capa do filme" 
+          style={{ width: '15%', height: 'auto',margin:'10px' }} // Definindo largura de 100% e altura automática
+        />
+
+        <img 
+          src="https://capas-p.imagemfilmes.com.br/164090_000_p.jpg" 
+          alt="Capa do filme" 
+          style={{ width: '15%', height: 'auto',margin:'10px' }} // Definindo largura de 100% e altura automática
+        />
+
+        <img 
+          src="https://capas-p.imagemfilmes.com.br/164834_000_p.jpg" 
+          alt="Capa do filme" 
+          style={{ width: '15%', height: 'auto',margin:'10px' }} // Definindo largura de 100% e altura automática
+        />
+
+
+
+
+
+
+
+
+        </div>
+      </div>    
     </div>
-
-  )
+  );
 }
